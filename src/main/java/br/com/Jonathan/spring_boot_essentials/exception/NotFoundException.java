572240d703d2257d3c441feaf9e1c697a88ce0a9
Jonathan0550 <jonathanaragao0550@gmail.com>
2026-06-21 18:42:40 -0300

    FIM DA AULA 1

---
+++ src/main/java/br/com/Jonathan/spring_boot_essentials/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.Jonathan.spring_boot_essentials.exception;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message){
+        super(message);
+    }
+
+}
